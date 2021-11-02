1f69b781af9e747c309db9ebd233e4dd92307d8e
Juanse1595 <3309@holbertonschool.com>
2021-11-02 19:48:59 +0000

    UPD: Updating test_base_model with isinstance test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,6 +5,7 @@
 from unittest import TestCase
 from models import base_model
 import pycodestyle
+BaseModel = base_model.BaseModel
 
 
 class Test_style(TestCase):
@@ -19,3 +20,15 @@ class Test_style(TestCase):
             'models/base_model.py'])
         self.assertEqual(foo.total_errors, 0,
                          "Found code style error (and warnings).")
+
+class Test_base(TestCase):
+    """[Class for testing all the function of base class]
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.base_test1 = BaseModel()
+
+    def test_id_values(self):
+        """[Testing values of ids]
+        """
+        self.assertIsInstance(self.base_test1, BaseModel)
