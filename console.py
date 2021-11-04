2b478441ea530d7b7c3155907cfc0211696e3bd6
Juanse1595 <3309@holbertonschool.com>
2021-11-04 23:08:34 +0000

    UPD: adding imports of all new classes

--- console.py
+++ console.py
@@ -4,10 +4,15 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 import shlex
 
-classes = {'BaseModel', 'User'}
+classes = {'BaseModel', 'User', 'Place', 'State', 'City', 'Amenity', 'Review'}
 
 
 class HBNBCommand(cmd.Cmd):
