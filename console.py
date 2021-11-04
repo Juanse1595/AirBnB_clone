5d587ec492a344e54e7dff4757dd9f601cca7ab9
Juanse1595 <3309@holbertonschool.com>
2021-11-04 16:38:21 +0000

    UPD: Changing classes set from TestModel to User

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel
 from models import storage
 import shlex
 
-classes = {'BaseModel', 'TestModel'}
+classes = {'BaseModel', 'User'}
 
 
 class HBNBCommand(cmd.Cmd):
