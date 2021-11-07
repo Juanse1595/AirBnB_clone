8fe17b08e8f206c0def8c34d133942b3df4d083e
JuanSebastianGB <3393@holbertonschool.com>
2021-11-07 01:48:35 +0000

    UPL: Uploading files to implement cosole's tests

---
+++ tests/test_console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""[Unittest for base_model]"""
+from datetime import date, datetime
+from unittest import TestCase
+from models import base_model
+import uuid
+import pycodestyle
+
+
+class Test_style(TestCase):
+    """[Class created to test style and syntax requirements for the
+    console]
+    """
+
+    def test_pycode(self):
+        """[Function that check Syntax from Peep8 branch called pycodestyle] 
+        """
+        foo = pycodestyle.StyleGuide(quiet=True).check_files([
+            'models/base_model.py'])
+        self.assertEqual(foo.total_errors, 0,
+                         "Found code style error (and warnings).")
+
+
+class Test_console(TestCase):
+    """[Class for testing console]"""
+    @classmethod
+    def setUpClass(cls):
+        """Setting up a test object"""
+        pass
