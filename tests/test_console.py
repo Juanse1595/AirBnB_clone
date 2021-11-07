671662e1ed29ece0a82207ff213418972dcd81ca
Juanse1595 <3309@holbertonschool.com>
2021-11-07 16:44:49 +0000

    Changing path of file in test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -19,7 +19,7 @@ class Test_style(TestCase):
         """[Function that check Syntax from Peep8 branch called pycodestyle]
         """
         foo = pycodestyle.StyleGuide(quiet=True).check_files([
-            'models/base_model.py'])
+            'console.py'])
         self.assertEqual(foo.total_errors, 0,
                          "Found code style error (and warnings).")
 
